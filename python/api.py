af92035b436c86e62729fe2a1984788ec6111692
Raquel Navarro <raquelnavarro@MacBook-Air-de-Raquel.local>
2023-04-30 12:35:40 -0600

    starting the database part of the project

--- python/api.py
+++ python/api.py
@@ -1,6 +1,11 @@
 import json
 import mysql.connector
-from flask import Flask
+from flask import *
+import os
+ 
+# Define allowed files
+ALLOWED_EXTENSIONS = {'csv'}
+
 app = Flask(__name__)
 
 connection = mysql.connector.connect(
@@ -9,5 +14,19 @@ connection = mysql.connector.connect(
 @app.route('/', methods=['GET'])
 def index():
     return "Service up"
+
+@app.route('/load', methods=['POST'])
+def load():
+    #data_to_load would be a S3 key in a bucket
+    if ('table' in request.args.keys() and 'file' in request.args.keys()):
+        table = request.args['table']
+        location =  'data_to_load/' + request.args['file'] + '.csv'
+        create_table_statement = 'create table ' + table + 
+
+        return table + ' ' + location
+    else:
+        return "Arguments 'table' and 'location' are required"
+
+
 if __name__ == "__main__":
     app.run(debug = True, host='0.0.0.0', port=7007)
